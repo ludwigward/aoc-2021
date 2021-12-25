429be96ebaf0d41a7bd87c5ebbcb621918bbd08a
Ludwig Ward <ludwig.ward@yubico.com>
2021-12-24 22:07:53 -0800

    day1, part 2

--- day1.go
+++ day1.go
@@ -14,6 +14,11 @@ func check(e error) {
 }
 
 func main() {
+	partA()
+	partB()
+}
+
+func partA() {
 	file, err := os.Open("./input_day1.txt")
 	check(err)
 	defer file.Close()
@@ -37,5 +42,37 @@ func main() {
 		previous = next
 	}
 
-	fmt.Println("Increases:", increases)
+	fmt.Println("Increases part 1: ", increases)
+}
+
+func partB() {
+	file, err := os.Open("./input_day1.txt")
+	check(err)
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+
+	var row, tmp, previous, next, increases int
+
+	values := make([]int, 0)
+
+	for scanner.Scan() {
+		tmp, err = strconv.Atoi(scanner.Text())
+		values = append(values, tmp)
+	}
+
+	for row < len(values) {
+		if row > 2 {
+			previous = values[row-3] + values[row-2] + values[row-1]
+			next = values[row-2] + values[row-1] + values[row]
+
+			if next > previous {
+				increases++
+			}
+		}
+
+		row++
+	}
+
+	fmt.Println("Increases part 2: ", increases)
 }
